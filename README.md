87bd449cc05a83bdc79b68010337d4ad745fae7f
Shadwell Da Cunha <contact@sdacunha.com>
2023-03-21 16:09:27 -0400

    Add features to README.md

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # mongoose-paginate
 Cursor based pagination for Mongoose
 
+# Features
+- Support for mongoose and @typegoose/typegoose
+- Multiple sort fields
+- Next and previous cursors
+- Aggregation pipelines
+
 # Credits
 Initial inspiration for this project began with https://github.com/ExtraBB/typegoose-cursor-pagination. I forked this and soon realized there were a few gaps I wanted to cover (support for multiple sort fields, support for aggregation pipelines), and some different design choices needed to be made.
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,16 +2,16 @@ import { encode, decode } from "../src/utils"
 
 describe("encode", () => {
   test("it encodes an empty string successfully", () => {
-    expect(encode("")).toBe("IiI")
+    expect(encode("")).toBe("IiI") // This is equal to []
   })
   test("it encodes a mongo ObjectId successfully", () => {
     expect(encode("5349b4ddd2781d08c09890f3")).toBe("IjUzNDliNGRkZDI3ODFkMDhjMDk4OTBmMyI")
-  })
+  }) // This is equal to ["5349b4ddd2781d08c09890f3"]
 })
 
 describe("decode", () => {
-  test("it throws error for an empty string", () => {
-    expect(decode("")).toEqual([])
+  test("it returns an empty array for an empty string", () => {
+    expect(decode("")).toEqual([]) // This isn't a case we should encounter in the library since we always try to use _id
   })
 
   test("it decodes a mongo ObjectId successfully", () => {
